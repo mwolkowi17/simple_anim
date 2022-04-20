eaa1ccfc81cb94d484c8b4ce2f93315fea48acaa
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-04-20 16:57:57 +0200

    animated blocks

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,6 +6,7 @@ import { Light2 } from './light2'
 import { Controls1 } from './controls1'
 import { Loader1 } from './loader1'
 
+
 export const scene = new THREE.Scene()
 
 const light1 = new Light1()
@@ -17,7 +18,7 @@ scene.add(light2.light)
 const camera1 = new Camera1()
 scene.add(camera1.camera)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -33,6 +34,39 @@ function onWindowResize() {
     render()
 }
 
+//trigs
+
+const trig1= document.createElement('div')
+trig1.className='tra'
+document.body.appendChild(trig1)
+trig1.addEventListener('mouseover', function(){loader1.play1()})
+
+const trig2= document.createElement('div')
+trig2.className='trb'
+document.body.appendChild(trig2)
+trig2.addEventListener('mouseover', function(){loader1.play2()})
+
+const trig3= document.createElement('div')
+trig3.className='trc'
+document.body.appendChild(trig3)
+trig3.addEventListener('mouseover', function(){loader1.play3()})
+
+const trig4= document.createElement('div')
+trig4.className='trd'
+document.body.appendChild(trig4)
+trig4.addEventListener('mouseover', function(){loader1.play4()})
+
+const trig5= document.createElement('div')
+trig5.className='tre'
+document.body.appendChild(trig5)
+trig5.addEventListener('mouseover', function(){loader1.play5()})
+
+const trig6= document.createElement('div')
+trig6.className='trf'
+document.body.appendChild(trig6)
+trig6.addEventListener('mouseover', function(){loader1.play6()})
+
+
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
@@ -43,7 +77,7 @@ function animate() {
 
     controls1.main.update()
 
-    if (loader1.mixer2) loader1.mixer2.update(clock.getDelta())
+    if (loader1.mixer) loader1.mixer.update(clock.getDelta())
 
     render()
 
--- src/client/loader1.ts
+++ src/client/loader1.ts
@@ -4,18 +4,74 @@ import { scene } from './client'
 
 export class Loader1 {
     main: GLTFLoader
-    mixer2: any
+    mixer: any
+    action: any
+    action1: any
+    action2: any
+    action3: any
+    action4: any
+    action5: any
+   
 
     constructor() {
         this.main = new GLTFLoader()
         this.main.load('models/deform1.gltf', (gltf) => {
             gltf.scene.position.set(0, 1, 0)
             gltf.scene.rotation.set(Math.PI / 2.2, 0, 0)
-            this.mixer2 = new THREE.AnimationMixer(gltf.scene)
-            const action = this.mixer2.clipAction(gltf.animations[0])
-            console.log(action)
-            action.play()
+            this.mixer = new THREE.AnimationMixer(gltf.scene)
+            this.action = this.mixer.clipAction(gltf.animations[0])
+            this.action1 = this.mixer.clipAction(gltf.animations[1])
+            this.action2 = this.mixer.clipAction(gltf.animations[2])
+            this.action3 = this.mixer.clipAction(gltf.animations[3])
+            this.action4 = this.mixer.clipAction(gltf.animations[4])
+            this.action5 = this.mixer.clipAction(gltf.animations[5])
+            console.log(this.action)
+            //this.action.play()
+            //this.action1.play()
             scene.add(gltf.scene)
         })
     }
+
+    play1(){
+        
+        this.action.play()
+        setTimeout(() => {
+             this.action.stop()}, 4000);
+    }
+
+    play2(){
+       
+        this.action1.play()
+        setTimeout(() => {
+             this.action1.stop()}, 4000);
+    }
+
+    play3(){
+        
+        this.action2.play()
+        setTimeout(() => {
+             this.action2.stop()}, 4000);
+    }
+
+    play4(){
+        console.log('clicked')
+        this.action3.play()
+        setTimeout(() => {
+             this.action3.stop()}, 4000);
+    }
+
+    play5(){
+        console.log('clicked')
+        this.action4.play()
+        setTimeout(() => {
+             this.action4.stop()}, 4000);
+    }
+
+    play6(){
+        console.log('clicked')
+        this.action5.play()
+        setTimeout(() => {
+             this.action5.stop()}, 4000);
+    }
+
 }
